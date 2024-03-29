3b554d58a998648378dd3259c5c29faa84d9cefc
Teguono <mumtes13@gmail.com>
2024-03-29 01:07:53 -0500

    testing token expiry

--- backend/utils/jwtUtils.js
+++ backend/utils/jwtUtils.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
 
 exports.generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1s" });
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "4h" });
 };
